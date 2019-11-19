81d19d2740d7542399a6e8972af3bc1f1835c6c7
rileybrady <37843519+rileybrady@users.noreply.github.com>
2019-11-19 15:14:59 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,160 @@
+
+//make variable for audio file. Ex: x.play();
+var a = document.getElementById("audio1");
+var b = document.getElementById("audio2");
+var c = document.getElementById("audio3");
+var d = document.getElementById("audio4");
+var e = document.getElementById("audio5");
+var f = document.getElementById("audio6");
+var g = document.getElementById("audio7");
+var h = document.getElementById("audio8");
+var i = document.getElementById("audio9");
+var j = document.getElementById("audio10");
+var k = document.getElementById("audio11");
+var l = document.getElementById("audio12");
+var m = document.getElementById("audio13");
+var n = document.getElementById("audio14");
+var o = document.getElementById("audio15");
+var p = document.getElementById("audio16");
+var q = document.getElementById("audio17");
+var r = document.getElementById("audio18");
+var s = document.getElementById("audio19");
+var t = document.getElementById("audio20");
+var u = document.getElementById("audio21");
+var v = document.getElementById("audio22");
+var w = document.getElementById("audio23");
+
+function playAudio() {
+  var x = document.getElementById("audio1");
+  a.play();
+}
+
+function playAudio() {
+  var x = document.getElementById("audio2");
+  b.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio3");
+  c.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio4");
+  d.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio5");
+  e.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio6");
+  x.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio7");
+  f.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio8");
+  g.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio9");
+  h.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio10");
+  i.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio11");
+  j.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio12");
+  k.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio13");
+  l.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio14");
+  m.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio15");
+  n.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio16");
+  o.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio17");
+  p.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio18");
+  q.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio19");
+  r.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio20");
+  s.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio21");
+  t.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio22");
+  u.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio23");
+  v.play();
+}
+
+function playAudio() {
+	var x = document.getElementById("audio24");
+  w.play();
+}
+
+function toggleText() {
+	if(i = 1) { 
+	document.getElementById("audio1").style.display = "none"
+	document.getElementById("audio2").style.display = "none"
+	document.getElementById("audio3").style.display = "none"
+	
+	
+	document.getElementById("text1").classList.remove("display-none");
+	document.getElementById("text2").classList.remove("display-none");	
+	document.getElementById("text3").classList.remove("display-none");
+	}
+
+}
+
